33a1f524dbcd96b5be75092caf49268ad4329024
anupamrajput82 <33576438+anupamrajput82@users.noreply.github.com>
2023-10-03 10:26:01 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -34,11 +34,27 @@ resource "google_container_cluster" "primary" {
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
-}
 
-# Separately Managed Node Pool
-resource "google_container_node_pool" "primary_nodes" {
-  name       = google_container_cluster.primary.name
+private_cluster_config {
+    enable_private_endpoint = true
+    enable_private_nodes   = true 
+    master_ipv4_cidr_block = "10.13.0.0/28"
+  }
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = "10.11.0.0/21"
+    services_ipv4_cidr_block = "10.12.0.0/21"
+  }
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "10.0.0.7/32"
+      display_name = "net1"
+    }
+
+}
+}
+# Separately Managed First Node Pool
+resource "google_container_node_pool" "apigee-data" {
+  name       = "apigee-data"
   location   = var.region
   cluster    = google_container_cluster.primary.name
   
@@ -56,7 +72,41 @@ resource "google_container_node_pool" "primary_nodes" {
     }
 
     # preemptible  = true
-    machine_type = "n1-standard-1"
+    machine_type = "e2-micro"
+    disk_type = "pd-standard"
+    disk_size_gb = 10
+    image_type = "UBUNTU_CONTAINERD"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+
+# Separately Managed Second Node Pool
+resource "google_container_node_pool" "apigee-runtime" {
+  name       = "apigee-runtime"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  
+  version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
+  node_count = var.gke_num_nodes
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    # preemptible  = true
+    machine_type = "e2-micro"
+    disk_type = "pd-standard"
+    disk_size_gb = 10
+    image_type = "UBUNTU_CONTAINERD"
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
