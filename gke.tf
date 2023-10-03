afc35873a7db7d1adc2eb66937e971492867cb9c
anupamrajput82 <33576438+anupamrajput82@users.noreply.github.com>
2023-10-03 14:23:37 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -35,23 +35,24 @@ resource "google_container_cluster" "primary" {
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
 
-private_cluster_config {
-    enable_private_endpoint = true
-    enable_private_nodes   = true 
-    master_ipv4_cidr_block = "10.13.0.0/28"
-  }
-  ip_allocation_policy {
-    cluster_ipv4_cidr_block  = "10.11.0.0/21"
-    services_ipv4_cidr_block = "10.12.0.0/21"
-  }
-  master_authorized_networks_config {
-    cidr_blocks {
-      cidr_block   = "10.0.0.7/32"
-      display_name = "net1"
-    }
+#private_cluster_config {
+#    enable_private_endpoint = true
+#    enable_private_nodes   = true 
+#    master_ipv4_cidr_block = "10.13.0.0/28"
+#  }
+#  ip_allocation_policy {
+#    cluster_ipv4_cidr_block  = "10.11.0.0/21"
+#    services_ipv4_cidr_block = "10.12.0.0/21"
+#  }
+#  master_authorized_networks_config {
+#    cidr_blocks {
+#      cidr_block   = "10.0.0.7/32"
+#      display_name = "net1"
+#    }
+#}
 
 }
-}
+
 # Separately Managed First Node Pool
 resource "google_container_node_pool" "apigee-data" {
   name       = "apigee-data"
